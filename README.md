e7319ccc17a8da13676890e98f1786a9325e9726
Sergey Burmisov <sb@convexitydmcc.com>
2022-04-05 09:39:40 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Нужен установленный node.js (я проверяю на node.js 16)
+
+```
+npm install
+npx hardhat test
+```
---
+++ contracts/Token.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+contract Token {
+    string public name = "My Token";
+    string public symbol = "TKN";
+
+    uint256 public totalSupply = 10000;
+
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
